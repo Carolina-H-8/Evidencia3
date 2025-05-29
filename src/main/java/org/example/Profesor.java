a62a066020de11c327dcc04aa7f2bd37f6e9fb70
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-05-28 22:31:02 -0500

    feat: TDA clase profesor

--- src/main/java/org/example/Profesor.java
+++ src/main/java/org/example/Profesor.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Profesor extends Persona {
-    String Curso;
+    private static Profesor[] listaProfesores = new Profesor[50];
+    private static int contador = 0;
+    private String Curso;
 
     public Profesor(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
         super(nombre, apellido, fechaNacimiento, genero, estatura, peso);
         this.Curso = Curso;
     }
+
+    public void TDA(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
+        if (contador < listaProfesores.length) {
+            listaProfesores[contador] = new Profesor(nombre, apellido, fechaNacimiento, genero, estatura, peso);
+            contador++;
+        }else {
+            System.out.printf("Limite de datos alcanzado");
+        }
+    }
+
+
+
 
 
 
