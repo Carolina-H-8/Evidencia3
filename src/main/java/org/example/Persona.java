dcbf9bf3d47c0bf5589ab64124b1b65152c5816f
Carolina-H-8 <astrid.hernandez@est.iudigital.edu.co>
2025-05-28 21:52:51 -0500

    feat: Iniciando clase persona

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String fechaNacimiento;
+    protected String genero;
+    protected double estatura;
+    protected double peso;
+
+}
