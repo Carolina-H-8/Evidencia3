0492f1a1a0cb57fccc959bb773b43e9c18e64f66
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-05-28 22:50:53 -0500

    feat: Prueba de datos en TDA

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Profesor.TDA("Laura", "Martínez", "1985-06-22", "F", 1.65, 60.0, "Matemáticas");
+        Profesor.TDA("José", "Ramírez", "1979-11-15", "M", 1.80, 80.0, "Física");
 
+        Profesor.mostarDatos();
 
 
     }
--- src/main/java/org/example/Profesor.java
+++ src/main/java/org/example/Profesor.java
@@ -1,23 +1,27 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class Profesor extends Persona {
-    private static Profesor[] listaProfesores = new Profesor[50];
+    private static Profesor[] listaProfesores = new Profesor[2];
     private static int contador = 0;
     private String Curso;
 
-    public Profesor(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
+    public Profesor(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso, String Curso) {
         super(nombre, apellido, fechaNacimiento, genero, estatura, peso);
         this.Curso = Curso;
     }
 
-    public void TDA(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
+    public static void TDA(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso, String Curso) {
         if (contador < listaProfesores.length) {
-            listaProfesores[contador] = new Profesor(nombre, apellido, fechaNacimiento, genero, estatura, peso);
+            listaProfesores[contador] = new Profesor(nombre, apellido, fechaNacimiento, genero, estatura, peso, Curso);
             contador++;
         }else {
             System.out.printf("Limite de datos alcanzado");
+        }
+    }
+    public static void mostarDatos() {
+        for(int i=0; i<listaProfesores.length; i++){
+            Profesor p = listaProfesores[i];
+            System.out.println(p.getNombre() +"Curso : "+p.Curso);
         }
     }
 
