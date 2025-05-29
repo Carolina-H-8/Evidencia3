4fae472af3de4f81a547626279fa1238048423dd
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-05-28 22:09:31 -0500

    feat: Constructor clase Profesor

--- src/main/java/org/example/Profesor.java
+++ src/main/java/org/example/Profesor.java
@@ -3,6 +3,10 @@ package org.example;
 public class Profesor extends Persona {
     String Curso;
 
+    public Profesor(String nombre, String apellido, String fechaNacimiento, String genero, double estatura, double peso) {
+        super(nombre, apellido, fechaNacimiento, genero, estatura, peso);
+        this.Curso = Curso;
+    }
 
 
 
